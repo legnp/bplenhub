6791ebe50dfc84b36f4780a67d8993a2ab45596a
legnp <legnp@bplen.com>
2026-03-14 11:05:14 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Portal BPlen Hub</h1>
+      <p>O sistema está sendo conectado à sua planilha...</p>
+    </div>
+  )
+}
