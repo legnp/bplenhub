9bef22e1478d76c48357ec7681bfb52de294ee1c
legnp <legnp@bplen.com>
2026-04-03 03:26:08 -0300

    correção deploy 2123662

--- src/actions/social.ts
+++ src/actions/social.ts
@@ -14,6 +14,7 @@ import {
   Timestamp
 } from "firebase/firestore";
 import { db } from "@/lib/firebase";
+import { SocialPost } from "@/types/social";
 import { revalidatePath } from "next/cache";
 import { requireAdmin } from "@/lib/auth-guards";
 
